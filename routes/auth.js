aed3fb992562408212c7b415939f1d10cef564c1
CHIRAG <cgupta_be21@thapar.edu>
2023-06-14 14:25:23 +0530

    validator added

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
 //Ceerate a user using Post "/api/auth/". Doesn't require auth
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save()
-    res.send("hello");
+router.post('/',[
+    body('name',"Enter a valid name").isLength({min:3}),
+    body('email',"Enter a valid email").isEmail(),
+    body('password',"password must be atleast 8 characters").isLength({min:8})
+],(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user=>res.json(user))
+    .catch(err=>{console.log(err)
+    res.json({error:"please Enter correct value"})})
+    // res.send("hello");
 })
 
 module.exports = router
